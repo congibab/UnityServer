55cc3978a1fa655fdcf21919d1fe54efd0c4bd9e
congibab <engljapen2@gmail.com>
2020-11-13 02:24:21 +0900

    Server.js base2

--- servrt/Server.js
+++ servrt/Server.js
@@ -3,15 +3,39 @@ var wss = new WebSocketServer({
     port: 8090
 });
 
-wss.on('connection', function(ws){
+var connections = [];
+
+wss.on('connection', function connection(ws, req){
     console.log('some user connected');
-    ws.on('message', function(message){
+    connections.push(ws);
+
+    const ip = req.socket.remoteAddress;
+    console.log(ip);
+
+    //===================================================
+    ws.on('close', function(){
+        connections = connections.filter(function(conn, i){
+            return (conn === ws) ? false : true;
+        });
+    });
+
+    ws.on('message', function incoming(message){
         console.log('received : %s', message)
-        
+        console.log(ws);
+
         wss.clients.forEach(function(client){
+            if(client !== ws){
                 client.send(message);
+            }
         });
         //ws.send(message);
     })
+    //===================================================
     ws.send('message sended from server');
 })
+
+function broadcast(message){
+    connections.forEach(function (con, i){
+        con.send(message);
+    });
+};
