6a27b16c68c1dc6533fd66343df21f0a70c0b240
congibab <engljapen2@gmail.com>
2021-02-18 01:31:08 +0900

    test

--- docker_nodejs/app.js
+++ docker_nodejs/app.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const app = express() 
-app.get('/', (req, res) => res.send('Hello World')); 
+app.get('/', (req, res) => res.send('Hello World test')); 
 
 app.listen(3000, () => { 
 	console.log('My REST API running on port 3000!');
